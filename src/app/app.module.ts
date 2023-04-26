d9052feeabcf46caa69930415dc430b0ab2e12c1
Giulia <giuliaportosilva@gmail.com>
2023-04-25 21:56:15 -0300

    feat: Create customer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,13 @@ import {MatDividerModule} from '@angular/material/divider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { CustomerService } from './customer.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule,
@@ -20,7 +23,9 @@ import { AppComponent } from './app.component';
     MatButtonModule,
     MatDividerModule
   ],
-  providers: [],
+  providers: [
+    CustomerService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
